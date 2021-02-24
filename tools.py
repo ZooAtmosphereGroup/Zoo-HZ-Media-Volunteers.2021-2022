0873e9abb6c7a6eebf9645bca0228f6798a891d2
zbb <1q2w3e4r5>
2021-02-24 11:59:21 +0800

    type: init level: normal comment: add test images

--- tools.py
+++ tools.py
@@ -4,37 +4,48 @@ from PIL import Image
 
 
 class HelloPhoto(object):
+    url_home = 'https://zooatmospheregroup.github.io/Zoo-HZ-Media-Volunteers.2021-2022/'
 
     def __init__(self):
         pass
 
     @staticmethod
-    def transfer_jpg_to_webp(path):
+    def mkdir_if_not_exist(path):
+        if os.path.exists(path) and os.path.isdir(path):
+            return
+        os.makedirs(path)
 
-        def _do(path_file):
-            print('transfer_jpg_to_webp', path_file)
+    @classmethod
+    def transfer_jpg_to_webp(cls, path_in, path_out):
+        cls.mkdir_if_not_exist(path_out)
+
+        def _do(_path_file):
+            print('transfer_jpg_to_webp', _path_file)
             try:
-                Image.open(path_file).convert("RGB").save(path_file + '.webp', 'WEBP')
+                _, _f = os.path.split(_path_file)
+                _path = os.path.join(path_out, _f)
+                Image.open(_path_file).convert("RGB").save(_path + '.webp', 'WEBP')
             except Exception as e:
-                print("cannot convert", path_file, e)
+                print("cannot convert", _path_file, e)
 
         # file
-        if os.path.isfile(path):
-            _do(path)
+        if os.path.isfile(path_in):
+            _do(path_in)
             return
 
         # folder
-        if os.path.isdir(path):
-            for f in os.listdir(path):
-                _do(os.path.join(path, f))
+        if os.path.isdir(path_in):
+            for f in os.listdir(path_in):
+                _do(os.path.join(path_in, f))
             return
 
         # link or other
-        print('path: %s is a link or other file' % path)
+        print('path: %s is a link or other file' % path_in)
         return
 
-    @staticmethod
-    def create_thumbnail(path, size=(640, 360)):
+    @classmethod
+    def create_thumbnail(cls, path_in, path_out, size=(640, 360)):
+        cls.mkdir_if_not_exist(path_out)
 
         def _do(_path_file):
             print('create_thumbnail', _path_file)
@@ -53,34 +64,110 @@ class HelloPhoto(object):
                 region = im.crop(_size)
                 # region.resize(size)
                 region.thumbnail(size)
-                region.save(_path_file + '.webp', 'WEBP')
+                _, _f = os.path.split(_path_file)
+                _path = os.path.join(path_out, _f)
+                region.save(_path + '.webp', 'WEBP')
             except Exception as e:
                 print("cannot convert", _path_file, e)
 
         # file
-        if os.path.isfile(path):
-            _do(path)
+        if os.path.isfile(path_in):
+            _do(path_in)
             return
 
         # folder
-        if os.path.isdir(path):
-            for f in os.listdir(path):
-                _do(os.path.join(path, f))
+        if os.path.isdir(path_in):
+            for f in os.listdir(path_in):
+                _do(os.path.join(path_in, f))
             return
 
         # link or other
-        print('path: %s is a link or other file' % path)
+        print('path: %s is a link or other file' % path_in)
         return
 
-    @staticmethod
-    def create_markdown(path):
-        pass
+    @classmethod
+    def resize_with_the_same_ratio(cls, path_in, path_out, width_max=3200, height_max=3200):
+        cls.mkdir_if_not_exist(path_out)
 
-    @staticmethod
-    def renew_home_page(self):
+        def _do(_path_file):
+            print('resize_with_the_same_ratio', _path_file)
+            try:
+                im = Image.open(_path_file)
+                width, height = im.size
+
+                if width >= height:
+                    height = int(height / (width / width_max))
+                    width = width_max
+                else:
+                    width = int(width / (height / height_max))
+                    height = height_max
+
+                _, _f = os.path.split(_path_file)
+                _path = os.path.join(path_out, _f)
+                im.resize((width, height)).save(_path + '.3200.jpg', 'JPEG')
+            except Exception as e:
+                print("cannot convert", _path_file, e)
+
+        # file
+        if os.path.isfile(path_in):
+            _do(path_in)
+            return
+
+        # folder
+        if os.path.isdir(path_in):
+            for f in os.listdir(path_in):
+                _do(os.path.join(path_in, f))
+            return
+
+        # link or other
+        print('path: %s is a link or other file' % path_in)
+        return
+
+    @classmethod
+    def render_markdown(cls, path_in):
+        path_out = path_in.replace('/static/images/', '/markdowns/')
+        path_out_head, tail = os.path.split(path_out)
+        cls.mkdir_if_not_exist(path_out_head)
+
+        if not os.path.exists(path_in) or not os.path.isdir(path_in):
+            print('path: %s is not exist or is not dir' % path_in)
+            return
+
+        md_head = """---
+layout: default
+---
+### render for path: %s
+
+""" % path_in
+        md_item = """### {size}, {name}, {message}
+![{name}]({path})
+
+"""
+        md = ''
+        md += md_head
+        for f in os.listdir(path_in):
+            # /home/xxx/static/images/webp/zzz/a.webp
+            path_abs_f = os.path.join(path_in, f)
+            size = round(os.path.getsize(path_abs_f) / 1024 / 1024, 2)
+            size_str = 'size: %sM' % size
+
+            # url_home/static/images/webp/zzz/a.webp
+            path_relative_f = path_abs_f[path_abs_f.find('/static/images/'):]
+            path = cls.url_home + path_relative_f
+            item = md_item.format(size=size_str, name=f, path=path, message='')
+            md += item
+
+        with open(os.path.join(path_out_head, tail + '.md'), 'w') as f:
+            f.write(md)
+        return
+
+    @classmethod
+    def render_home_page(cls, path_in):
         pass
 
 
 if __name__ == '__main__':
-    HelloPhoto.transfer_jpg_to_webp('/home/Pictures')
+    # HelloPhoto.transfer_jpg_to_webp('/home/Pictures')
+    HelloPhoto.render_markdown(
+        '/home/z04014/Desktop/Cloud/Zoo-HZ-Media-Volunteers.2021-2022/static/images/webp/202102/test')
     # HelloPhoto.create_thumbnail('/home/Pictures')
